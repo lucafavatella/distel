f261fc4e82ea881e9a99effd148b8335a7f24358
lukeg <>
2002-07-29 13:13:01 +0000

    Switch keybindings prefix to "C-cC-d". Now have an "erlang-extended-mode" minor mode, which includes all extensions including edb.

--- elisp/erlang-mode-ext.el
+++ elisp/erlang-mode-ext.el
@@ -8,14 +8,18 @@
 ;; Debugger
 (require 'edb)
 
-;; Extended feature key bindings (C-x C-e prefix)
+;; Extended feature key bindings (C-x C-d prefix)
 
-(when (null erlang-mode-map)
-  (erlang-keymap-init))
-
-(define-key erlang-mode-map "\C-c\C-e:" 'erl-eval-expression)
-(define-key erlang-mode-map "\C-c\C-ep" 'fprof)
-(define-key erlang-mode-map "\C-c\C-e." 'erl-find-source-under-point)
-(define-key erlang-mode-map "\C-c\C-el" 'erl-process-list)
+(define-minor-mode erlang-extended-mode
+  "Minor mode extending erlang-mode."
+  nil
+  " EXT"
+  '(("\C-c\C-di" . edb-toggle-interpret)
+    ("\C-c\C-db" . edb-toggle-breakpoint)
+    ("\C-c\C-dm" . edb-monitor)
+    ("\C-c\C-d:" . erl-eval-expression)
+    ("\C-c\C-dp" . fprof)
+    ("\C-c\C-d." . erl-find-source-under-point)
+    ("\C-c\C-dl" . erl-process-list)))
 
 (provide 'erlang-mode-ext)
